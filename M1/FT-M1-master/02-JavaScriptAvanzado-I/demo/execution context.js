f31f14fb3d6ef09b84b5885587566e53d1b8b7c4
Jehoshym <josuorlen2010@gmail.com>
2022-09-26 20:48:06 -0300

    homework revision

--- M1/FT-M1-master/02-JavaScriptAvanzado-I/demo/execution context.js
+++ M1/FT-M1-master/02-JavaScriptAvanzado-I/demo/execution context.js
@@ -14,3 +14,20 @@ function     person() {
     } 
     alert(sayHello + ' ' + firstName() + ' ' + lastName());
 }
+
+var fullname = 'Juan Perez';
+var obj = {
+   fullname: 'Natalia Nerea',
+   prop: {
+      fullname: 'Aurelio De Rosa',
+      getFullname: function() {
+         return this.fullname;
+      }
+   }
+};
+
+console.log(obj.prop.getFullname()); //Aurelio de Rosa
+
+var test = obj.prop.getFullname;
+
+console.log(test());                    
